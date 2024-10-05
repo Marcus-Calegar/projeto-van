16005660b19f79edac827f3a3b7bb8593ddc6a06
mav-26 <calegarmarcus@gmail.com>
2024-10-05 16:20:39 -0300

    correcao bug

--- Controller/SolicitacaoController.php
+++ Controller/SolicitacaoController.php
@@ -6,11 +6,14 @@ if (isset($_GET)) {
     $motorista = $_GET['idMotorista'];
 
     if (Solicitacao::VerificarSolicitacoes($aluno, $responsavel, $motorista) <= 0) {
-
+        $enviar = new Solicitacao();
+        if ($enviar->EnviarSolicitacao($aluno, $responsavel, $motorista) == true)
+            echo "Solicitação enviada com sucesso!";
     } else {
         echo "Você já enviou uma solicitação desse aluno para este motorista.";
+        echo '</br>
+                <a href="../View/Pages/Logado.php">Voltar</a>';
     }
-    var_dump(Solicitacao::VerificarSolicitacoes($aluno, $responsavel, $motorista));
 } else {
     header('Location: ../View/Pages/PerfilMotorista.php');
 }
